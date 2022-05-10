a929574069d5fa4c70c4dbf75179b4d2d1332b85
kate <vuk_kat@mail.ru>
2022-05-10 18:30:30 +0500

    add Test

--- src/test/java/ru/netology/Test.java
+++ src/test/java/ru/netology/Test.java
@@ -1,5 +1,24 @@
 package ru.netology;
 
-public class Test {
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
+ class PostmanEchoApiTest {
+    @Test
+    public void shouldReturnBody() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
 }
